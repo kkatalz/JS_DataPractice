14c0f18fed8bb6cc2fd07ec667cd6588d27d4eb8
kkatalz <zlata.kiev2004@gmail.com>
2023-10-03 23:08:24 +0300

    Implemented second task.

--- main.js
+++ main.js
@@ -40,8 +40,34 @@ function getYearsFromDate(birthday) {
   return age;
 }
 
-function convertRandomUserMock(randomUsers) {
-  const mappedRandomUsers = randomUsers.map((user) => {
+//functions for second task
+function checkUppercase(string) {
+  if (typeof string === "string" && string[0]) {
+    const isFirstLetterUppercase = string[0] !== string[0].toLowerCase();
+    return isFirstLetterUppercase;
+  }
+}
+
+function checkLowercase(string) {
+  if (typeof string === "string" && string[0]) {
+    const isFirstLetterUppercase = string[0] !== string[0].toUpperCase();
+    return isFirstLetterUppercase;
+  }
+}
+
+function checkAge(age) {
+  if (typeof age === "number") return true;
+  else {
+    return false;
+  }
+}
+
+function checkPhoneNumber(phone, country) {}
+
+// 1 task
+
+function convertRandomUserMock(usersFirst, usersSecond) {
+  const mappedRandomUsers = usersFirst.map((user) => {
     return {
       gender: user.gender,
       title: user.name.title,
@@ -66,7 +92,7 @@ function convertRandomUserMock(randomUsers) {
     };
   });
 
-  const mappedAdditionalUsers = additionalUsers.map((user) => {
+  const mappedAdditionalUsers = usersSecond.map((user) => {
     return {
       gender: user.gender,
       title: user.name && user.name.title ? user.name.title : null, // first step: check if name exists in user, second step: check if title exists in name
@@ -99,4 +125,29 @@ function convertRandomUserMock(randomUsers) {
   return mappedUsersWithoutDuplicates;
 }
 
-console.log(convertRandomUserMock(randomUserMock));
+// 2 task
+
+function getValidUsers(users) {
+  const filteredUsers = users.filter(
+    (user) =>
+      checkUppercase(user.full_name) &&
+      checkLowercase(user.gender) &&
+      checkUppercase(user.note) &&
+      checkUppercase(user.state) &&
+      checkUppercase(user.city) &&
+      checkUppercase(user.country) &&
+      checkAge(user.age) &&
+      //   checkPhoneNumber(user.phone, user.country) &&
+      user.email.includes("@")
+  );
+
+  return filteredUsers;
+}
+
+// const array = [1,2,3,4,5];
+// const filteredArray = array.filter(value=>value!==3)
+// filteredArray -> [1,2,4,5]
+
+console.log(
+  getValidUsers(convertRandomUserMock(randomUserMock, additionalUsers))
+);
